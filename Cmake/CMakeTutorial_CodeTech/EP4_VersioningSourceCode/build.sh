8fb5e9c8f4dfa773e539a4b405fa432a42ed98c0
Sang Tan Truong <sang.tts.designer@gmail.com>
2023-08-23 19:14:29 +0700

    [CI] Add Automatic script for each Cmake project part 1
    DWP-18
    
    Change-Id: I4410558b96a4aa0edecc95e1746bd610553a02bc

--- Cmake/CMakeTutorial_CodeTech/EP4_VersioningSourceCode/build.sh
+++ Cmake/CMakeTutorial_CodeTech/EP4_VersioningSourceCode/build.sh
@@ -10,10 +10,11 @@ ECHO_HIGHLIGHT "Script path ==> $DIR" "BLUE"
 
 # apt-get install libxss-dev libxxf86vm-dev libxkbfile-dev libxv-dev libxrandr-dev libxinerama libxcursor libxi mesa-common-dev libglu1-mesa-dev freeglut3-dev
 
-CONFIGURE="cmake -DGLFW_BUILD_DOCS=OFF -S $DIR -B $DIR/out/build"
+CONFIGURE="cmake -S $DIR -B $DIR/out/build"
 BUILD="cmake --build $DIR/out/build"
 
 configure_cmake "$CONFIGURE"
 build_cmake "$BUILD"
 
 ECHO_HIGHLIGHT "BUILD SUCCESSFULLY" "LINE" "GREEN"
+
