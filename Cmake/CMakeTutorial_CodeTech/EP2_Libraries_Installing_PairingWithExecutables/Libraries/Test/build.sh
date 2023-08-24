022911f3bbb192550b580abf8c4efc4b5b3c7bf3
Sang Tan Truong <sang.tts.designer@gmail.com>
2023-08-24 17:09:31 +0700

    [CI] Add Automatic script for each Cmake project part 2
    DWP-18
    
    Change-Id: Idf8f47c9b3fa5b6821528c855ed61d25aaa597e9

--- Cmake/CMakeTutorial_CodeTech/EP2_Libraries_Installing_PairingWithExecutables/Libraries/Test/build.sh
+++ Cmake/CMakeTutorial_CodeTech/EP2_Libraries_Installing_PairingWithExecutables/Libraries/Test/build.sh
@@ -8,6 +8,14 @@ DIR=$(script_dir)
 
 ECHO_HIGHLIGHT "Script path ==> $DIR" "BLUE"
 
+ECHO_HIGHLIGHT "Build libraries" "CYAN"
+LIB_CONFIGURE="cmake -S $DIR/../AdderLib -B $DIR/../AdderLib/out/build"
+LIB_BUILD="cmake --build $DIR/../AdderLib/out/build"
+
+configure_cmake "$LIB_CONFIGURE"
+build_cmake "$LIB_BUILD"
+
+ECHO_HIGHLIGHT "Build Top CMakeLists.txt"
 CONFIGURE="cmake -S $DIR -B $DIR/out/build"
 BUILD="cmake --build $DIR/out/build"
 
