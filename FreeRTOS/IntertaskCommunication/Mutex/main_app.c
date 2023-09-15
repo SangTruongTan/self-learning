1d8ec7a9123056f4f40e55b47c7486d4a99145fc
Sang Tan Truong <sang.tts.designer@gmail.com>
2023-09-15 16:40:58 +0000

    [FreeRTOS] Correct mutex example
    DWP-33

--- FreeRTOS/IntertaskCommunication/Mutex/main_app.c
+++ FreeRTOS/IntertaskCommunication/Mutex/main_app.c
@@ -1,9 +1,9 @@
-/* FreeRTOS kernel includes. */
+/* FreeRTOS kernel include. */
 #include <FreeRTOS.h>
-#include <task.h>
 
-/* Must be greater than any priority used by the kernel and less than 32. */
-#define portINTERRUPT_YIELD 5
+/* FreeRTOS kernel modules include. */
+#include "semphr.h" /* Semaphore include file */
+#include <task.h>
 
 /* Primitive libraries. */
 #include <stdio.h>
@@ -13,37 +13,23 @@
 void vTask1(void *pvParameters);
 void vTask2(void *pvParameters);
 
-/* Handles for the tasks create by main_app(). */
-static TaskHandle_t xTask1 = NULL, xTask2 = NULL;
-
 /* Define the strings that will be passed in as the task parameters.
    They are to be printed to the console when tasks are running. */
 const char *pcTextForTask1 = "Task 1 is running\r\n";
 const char *pcTextForTask2 = "Task 2 is running\r\n";
 
-BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+/* Define the string as the shared data accessed by Task 1 and Task 2. */
+static char *sharedData = "It's the shared data.";
 
-static unsigned long prvProcessYieldInterrupt(void) {
-    /* xHigherPriorityTaskWoken must be initialised to pdFALSE. If calling
-    vTaskNotifyGiveFromISR() unblocks the handling task, and the priority of
-    the hanndling task is higher than the priority of the currently running
-    task, then xHigherPriorityTaskWoken will automatically get set to pdTRUE. */
-    xHigherPriorityTaskWoken = pdFALSE;
-
-    /* Send a notification to vTask2(), bringing it out of the Blocked state. */
-    vTaskNotifyGiveFromISR(xTask2, &xHigherPriorityTaskWoken);
-
-    printf((char *)"Notification was sent to Task2.\n");
-
-    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
-}
+/* Create the semaphore handle. */
+SemaphoreHandle_t xSemaphore1 = NULL;
 
 /*-----------------------------------------------------------*/
 
 int main_app(void) {
     /* Create tasks. */
-    xTaskCreate(vTask1, "Task 1", 100, (void *)pcTextForTask1, 1, &xTask1);
-    xTaskCreate(vTask2, "Task 2", 100, (void *)pcTextForTask2, 2, &xTask2);
+    xTaskCreate(vTask1, "Task 1", 100, (void *)pcTextForTask1, 1, NULL);
+    xTaskCreate(vTask2, "Task 2", 100, (void *)pcTextForTask2, 2, NULL);
 
     /* Start the scheduler to start the tasks executing. */
     vTaskStartScheduler();
@@ -52,32 +38,76 @@ int main_app(void) {
 /*-----------------------------------------------------------*/
 
 void vTask1(void *pvParameters) {
-    volatile uint32_t ul;
     char *pcTaskString;
     pcTaskString = (char *)pvParameters;
 
+    TickType_t xLastWakeTime;
+
+    /* The xLastWakeTime variable needs to be initialized with the current tick
+    count.  Note that this is the only time we access this variable.  From this
+    point on xLastWakeTime is managed automatically by the vTaskDelayUntil()
+    API function. */
+    xLastWakeTime = xTaskGetTickCount();
+
+    /* Create a sempahore for mutex. */
+    xSemaphore1 = xSemaphoreCreateMutex();
+
+    /* Check if the semaphore is created successfully. */
+    if (xSemaphore1 == NULL) {
+        configASSERT(pdFALSE);
+    }
     /* As per most tasks, this task is implemented in an infinite loop. */
     for (;;) {
-        vTaskDelay((TickType_t)500);
-        /* Print out the name of thii task. */
+        /* Print out the name of this task. */
         printf(pcTaskString);
 
-        /* Simulate the periodic interrupt handled by prvProccessYieldInterrupt.
-        */
-       portYIELD();
+        if (xSemaphore1 != NULL) {
+            /* Obtain the semaphore without blocking the task if the semaphore
+               isn't immediately available. */
+            if (xSemaphoreTake(xSemaphore1, (TickType_t)0)) {
+                /* Now the task can access the shared resource. Do something
+                 * here. */
+                sharedData = (char *)"Shared data changed by Task1";
+                if (strcmp(sharedData, "Shared data changed by Task1") != 0) {
+                    configASSERT(pdFALSE);
+                }
 
+                {
+                    printf("%s\r\n", sharedData);
+                    fflush(stdout);
+                }
+
+                /* Delay for a period of time. */
+                vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(300UL));
+
+                /* When finished accessing the shared resource, free the
+                   semaphore. */
+                if (xSemaphoreGive(xSemaphore1) != pdTRUE) {
+                    configASSERT(pdFALSE);
+                }
+            }
+        }
     }
 }
+
 /*-----------------------------------------------------------*/
 void vTask2(void *pvParameters) {
     char *pcTaskString;
     pcTaskString = (char *)pvParameters;
+
+    TickType_t xLastWakeTime;
+    xLastWakeTime = xTaskGetTickCount();
+
     printf(pcTaskString);
 
     /* As per most tasks, this task is implemented in an infinite loop. */
     for (;;) {
-        /* Blocks to wait for vTask1() to notify this task. */
-        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        printf((char *)"Task2 was notified!\n");
+        sharedData = (char *)"Shared data changed by Task2.";
+        {
+            printf("%s\r\n", sharedData);
+            fflush(stdout);
+        }
+        /* Delay for a period of time. */
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(500UL));
     }
 }
