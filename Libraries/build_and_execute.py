d2f2e50115a174ef5a68b236eaf734d6e6876247
Sang Tan Truong <sang.tts.designer@gmail.com>
2024-03-12 16:37:55 +0700

    [Python] Implement a automated script for C++
    SPF-97
    
    Change-Id: I29e50d3a10dbe5ef927f921dbb71a8c4ee8297f8

---
+++ Libraries/build_and_execute.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import os
+import sys
+
+# Colour printing
+def prRed(skk): print("\033[91m{}\033[00m" .format(skk))
+
+
+def prGreen(skk): print("\033[92m{}\033[00m" .format(skk))
+
+
+def prYellow(skk): print("\033[93m{}\033[00m" .format(skk))
+
+
+def prLightPurple(skk): print("\033[94m{}\033[00m" .format(skk))
+
+
+def prPurple(skk): print("\033[95m{}\033[00m" .format(skk))
+
+
+def prCyan(skk): print("\033[96m{}\033[00m" .format(skk))
+
+
+def prLightGray(skk): print("\033[97m{}\033[00m" .format(skk))
+
+
+def prBlack(skk): print("\033[98m{}\033[00m" .format(skk))
+
+def prWhite(skk): print(skk)
+
+def build_and_execute_cpp(file_name):
+    prGreen("Build the C++ program using g++")
+    compile_command = f"g++ {file_name} -o {file_name + '.o'}"
+    print(compile_command)
+    compile_status = os.system(compile_command)
+
+    if compile_status != 0:
+        prRed("Compilation failed.")
+        return
+
+    prGreen("Execute the compiled program")
+    execute_command = f"./{file_name + '.o'}"
+    os.system(execute_command)
+
+# Usage example
+if len(sys.argv) < 2:
+    prPurple("Please provide the C++ file as an argument.")
+else:
+    cpp_file = sys.argv[1]
+    build_and_execute_cpp(cpp_file)
