ff9393aac4c912165cf5fd653561e716f6945ae2
Sang Tan Truong <sang.truong@lge.com>
2024-05-29 15:50:24 +0700

    [Coding] Add assertion macro
    [Keys] SPF-108

--- Cpp/Algorithm/Sorting/HeapSort/Heaps/main.cc
+++ Cpp/Algorithm/Sorting/HeapSort/Heaps/main.cc
@@ -17,6 +17,21 @@
 #include <vector>
 // #include <bits/stdc++.h>
 
+#ifndef NDEBUG
+#define ASSERT(condition, message)                                             \
+    do {                                                                       \
+        if (!(condition)) {                                                    \
+            std::cerr << "Assertion `" #condition "` failed in " << __FILE__   \
+                      << " line " << __LINE__ << ": " << message << std::endl; \
+            std::terminate();                                                  \
+        }                                                                      \
+    } while (false)
+#else
+#define ASSERT(condition, message)                                             \
+    do {                                                                       \
+    } while (false)
+#endif
+
 // #define TEST
 
 struct MaxHeap {
