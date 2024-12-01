d148e5d6c59f403cfbb199346bf724c232c7b22f
Sang Tan Truong <sang.truong@outlook.com.vn>
2024-12-01 23:08:53 +0700

    [Engineering] Design Patterns - Command [Keys] SPF-99 [Category] Behavioral Pattern

---
+++ DesignPatterns/BehavioralPatterns/Command/Demo.java
@@ -0,0 +1,10 @@
+package DesignPatterns.BehavioralPatterns.Command;
+
+import DesignPatterns.BehavioralPatterns.Command.editor.Editor;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
