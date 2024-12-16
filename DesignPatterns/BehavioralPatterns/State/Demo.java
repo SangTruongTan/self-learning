31ee38a6daea7b48fb61657abf1ceef874427c61
Sang Tan Truong <sang.truong@outlook.com.vn>
2024-12-16 22:49:56 +0700

    [Engineering] Design Patterns - State [Keys] SPF-99 [Category] Behavioral Pattern

---
+++ DesignPatterns/BehavioralPatterns/State/Demo.java
@@ -0,0 +1,16 @@
+package DesignPatterns.BehavioralPatterns.State;
+
+import DesignPatterns.BehavioralPatterns.State.ui.Player;
+import DesignPatterns.BehavioralPatterns.State.ui.UI;
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
---
+++ DesignPatterns/BehavioralPatterns/State/states/LockedState.java
@@ -0,0 +1,40 @@
+package DesignPatterns.BehavioralPatterns.State.states;
+
+import DesignPatterns.BehavioralPatterns.State.ui.Player;
+
+/**
+ * Concrete states provide the special implementation for all interface methods.
+ */
+public class LockedState extends State {
+
+    LockedState(Player player) {
+        super(player);
+        player.setPlaying(false);
+    }
+
+    @Override
+    public String onLock() {
+        if (player.isPlaying()) {
+            player.changeState(new ReadyState(player));
+            return "Stop playing";
+        } else {
+            return "Locked...";
+        }
+    }
+
+    @Override
+    public String onPlay() {
+        player.changeState(new ReadyState(player));
+        return "Ready";
+    }
+
+    @Override
+    public String onNext() {
+        return "Locked...";
+    }
+
+    @Override
+    public String onPrevious() {
+        return "Locked...";
+    }
+}
